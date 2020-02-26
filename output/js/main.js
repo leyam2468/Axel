8e3087a04e63b71c3fe1fb1404bdcec62a334bab
Axel <leyamm3@gmail.com>
2020-02-26 15:39:45 +0200

    26/2 3:39
    Signed-off-by: Axel <leyamm3@gmail.com>

--- output/js/main.js
+++ output/js/main.js
@@ -1,7 +1,16 @@
+//-Nice Scroll--------------
+$("body").niceScroll({
+  cursorcolor:"#860000",
+  cursorwidth:"3px",
+  cursorborder:"none",
+  zindex: "999999",
+  scrollspeed: 60,
+});
+
 
  //--banner
 var swiper = new Swiper('.swiper1', {
-  speed: 600,
+  speed: 1000,
   parallax: true,
   pagination: {
     el: '.swiper-pagination',
@@ -21,17 +30,23 @@ var swiper = new Swiper('.swiper1', {
 var swiper1 = new Swiper('.swiper2', {
     effect: 'cube',
     grabCursor: true,
+    speed:1000,
     cubeEffect: {
       shadow: true,
       slideShadows: true,
       shadowOffset: 20,
       shadowScale: 0.94,
     },
+    autoplay: {
+      delay: 3000,
+      waitForTransition:true
+    },
   });
 
 
 //-badge Effects "coverflow"
   var swiper2 = new Swiper('.swiper3', {
+    speed:1000,
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
@@ -42,8 +57,12 @@ var swiper1 = new Swiper('.swiper2', {
       depth: 100,
       modifier: 1,
       slideShadows : true,
+      
     },
     Sliderpagination: {
       el: '.swiper-pagination',
+    },
+    autoplay: {
+      delay: 3000,
     },
 });
